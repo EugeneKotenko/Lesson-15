13c70714981a2f594c10fe425329b4a7e81b4280
EugeneKotenko <134332591+EugeneKotenko@users.noreply.github.com>
2023-06-26 22:49:17 +0300

    lesson-15

---
+++ index.js
@@ -0,0 +1,25 @@
+const table = document.getElementById('multiplicationTable');
+const tbody = table.getElementsByTagName('tbody')[0];
+
+const headerRow = table.tHead.rows[0];
+for (let i = 1; i <= 10; i++) {
+    const th = document.createElement('th');
+    th.textContent = i;
+    headerRow.appendChild(th);
+}
+
+for (let i = 1; i <= 10; i++) {
+    const row = document.createElement('tr');
+
+    const headerCell = document.createElement('th');
+    headerCell.textContent = i;
+    row.appendChild(headerCell);
+
+    for (let j = 1; j <= 10; j++) {
+        const cell = document.createElement('td');
+        cell.textContent = i * j;
+        row.appendChild(cell);
+    }
+
+    tbody.appendChild(row);
+}
